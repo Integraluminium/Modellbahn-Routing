fd38d8e0f6a6970eda2ccce1ffc1fc83fa10b765
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-11-21 11:50:10 +0100

    init

---
+++ src/test/java/de/dhbw/modellbahn/MainTest.java
@@ -0,0 +1,12 @@
+package de.dhbw.modellbahn;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
