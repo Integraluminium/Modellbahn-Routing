1d215944b36bf1781a1de878ed57b6e8942d78d1
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2025-01-18 15:42:07 +0100

    implemented timeout in API-Service

--- src/main/java/de/dhbw/modellbahn/adapter/moba/communication/ApiService.java
+++ src/main/java/de/dhbw/modellbahn/adapter/moba/communication/ApiService.java
@@ -15,18 +15,20 @@ import java.util.Objects;
 public class ApiService {
     private final String url;
     private final int senderHash;
+    private Duration timeout;
 
 
-    public ApiService(int senderHash, String url) {
+    public ApiService(int senderHash, String url, int timeout) {
         this.url = Objects.requireNonNull(url, "URL must not be be null");
         if (senderHash < 0) {
             throw new IllegalArgumentException("Sender hash must be positive");
         }
         this.senderHash = senderHash;
+        setTimeout(timeout);
     }
 
     public ApiService(int senderHash) {
-        this(senderHash, "http://127.0.0.1:8002");
+        this(senderHash, "http://127.0.0.1:8002", 10);
     }
 
     private static <T> String transformDtoToJson(T bodyObject) throws JsonProcessingException {
@@ -34,13 +36,13 @@ public class ApiService {
         return objectMapper.writeValueAsString(bodyObject);
     }
 
-    private static void sendHttpRequest(URI uri, String jsonBody) throws IOException, InterruptedException {
+    private static void sendHttpRequest(URI uri, String jsonBody, Duration timeout) throws IOException, InterruptedException {
         try (HttpClient client = HttpClient.newHttpClient()) {
             // Build the HttpRequest with the JSON body
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(uri)
                     .header("Content-Type", "application/json") // Set the content type to application/json
-                    .timeout(Duration.ofSeconds(10)) // Set a timeout for the request
+                    .timeout(timeout) // Set a timeout for the request
                     .POST(HttpRequest.BodyPublishers.ofString(jsonBody)) // Set the body
                     .build();
 
@@ -56,6 +58,13 @@ public class ApiService {
         }
     }
 
+    public void setTimeout(final int timeout) {
+        if (timeout < 1) {
+            throw new IllegalArgumentException("Timeout must be positive");
+        }
+        this.timeout = Duration.ofSeconds(timeout);
+    }
+
     public <T> void sendRequest(String locator, T bodyObject) {
         try {
             String jsonBody = transformDtoToJson(bodyObject);
@@ -63,7 +72,7 @@ public class ApiService {
             System.out.printf(jsonBody);
 
             URI uri = URI.create(url + locator);
-            sendHttpRequest(uri, jsonBody);
+            sendHttpRequest(uri, jsonBody, this.timeout);
 
         } catch (Exception e) {
             System.err.println("An error occurred: " + e.getMessage());
