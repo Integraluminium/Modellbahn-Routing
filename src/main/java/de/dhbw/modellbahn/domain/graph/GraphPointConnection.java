74a4583e264a284ff5967eaa9d1066be3b1ea356
MichalskyL <MichalskyL@gmx.de>
2025-01-27 15:50:01 +0100

    removed unused GraphPointConnection

--- src/main/java/de/dhbw/modellbahn/domain/graph/GraphPointConnection.java
+++
@@ -1,8 +0,0 @@
-package de.dhbw.modellbahn.domain.graph;
-
-public record GraphPointConnection(GraphPoint point1, GraphPoint point2) {
-
-    public boolean connects(GraphPoint p1, GraphPoint p2) {
-        return p1 == point1 && p2 == point2 || p1 == point2 && p2 == point1;
-    }
-}
