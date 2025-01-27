52dce2dadb72a4c071bf61eea582721870f7e27c
MichalskyL <MichalskyL@gmx.de>
2025-01-27 14:24:54 +0100

    added SwitchSide for differenciation between the incoming and outgoing side of a switch

--- src/main/java/de/dhbw/modellbahn/domain/graph/Switch.java
+++ src/main/java/de/dhbw/modellbahn/domain/graph/Switch.java
@@ -2,6 +2,8 @@ package de.dhbw.modellbahn.domain.graph;
 
 public interface Switch {
     void switchToConnectPoints(GraphPoint point1, GraphPoint point2) throws IllegalArgumentException;
+    void switchToConnectToPoint(GraphPoint point);
 
     boolean checkIfSwitchConnectsPoints(GraphPoint point1, GraphPoint point2);
+    SwitchSide getSwitchSideFromPoint(GraphPoint point);
 }
