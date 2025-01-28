d6756793ab9ec9019eb381ae51e12ec9a41bced5
MichalskyL <MichalskyL@gmx.de>
2025-01-28 14:55:12 +0100

    test for class Distance

--- src/main/java/de/dhbw/modellbahn/domain/graph/Distance.java
+++ src/main/java/de/dhbw/modellbahn/domain/graph/Distance.java
@@ -2,8 +2,8 @@ package de.dhbw.modellbahn.domain.graph;
 
 public record Distance(int value) {
     public Distance {
-        if (value < 0) {
-            throw new IllegalArgumentException("Distance value must be positive.");
+        if (value <= 0) {
+            throw new IllegalArgumentException("Distance value must be greater than 0.");
         }
     }
 }
