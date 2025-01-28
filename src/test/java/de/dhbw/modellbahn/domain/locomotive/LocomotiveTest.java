c227b550e18ef9f044a889e0e8c12cb92af8fdef
MichalskyL <MichalskyL@gmx.de>
2025-01-28 16:34:51 +0100

    improved LocomotiveTest

--- src/test/java/de/dhbw/modellbahn/domain/locomotive/LocomotiveTest.java
+++ src/test/java/de/dhbw/modellbahn/domain/locomotive/LocomotiveTest.java
@@ -1,27 +1,33 @@
 package de.dhbw.modellbahn.domain.locomotive;
 
 import de.dhbw.modellbahn.domain.ConfigReader;
+import de.dhbw.modellbahn.domain.graph.GraphPoint;
 import de.dhbw.modellbahn.plugin.JSONConfigReader;
 import org.junit.jupiter.api.BeforeAll;
-import de.dhbw.modellbahn.domain.graph.TrackContact;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LocomotiveTest {
+    private static final GraphPoint startPoint = new GraphPoint("startPoint");
+    private static final GraphPoint facingPoint = new GraphPoint("facingPoint");
     private static LocId locId;
+    private static Locomotive locomotive;
 
     @BeforeAll
-    static void createConfigReader() {
+    static void beforeAll() {
         ConfigReader configReader = new JSONConfigReader();
         locId = new LocId(16389, configReader);
+
+        locomotive = new Locomotive(locId, startPoint, facingPoint);
     }
 
     @Test
-    void testGetLocId() {
-        Locomotive loc = new Locomotive(locId, new TrackContact("startPoint"), new TrackContact("destination"));
-
-        assertEquals(loc.getLocId(), locId);
+    void testGetter() {
+        assertEquals(locomotive.getLocId(), locId);
+        assertEquals(locomotive.getCurrentPosition(), startPoint);
+        assertEquals(locomotive.getCurrentFacingDirection(), facingPoint);
+        assertEquals(locomotive.getCurrentSpeed(), new Speed(0));
     }
 
 }
