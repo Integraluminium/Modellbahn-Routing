3b6350e264e23051786be134cb70ce67cf63cd01
MichalskyL <MichalskyL@gmx.de>
2025-01-30 09:16:02 +0100

    refactored path of the config file containing the loc ids

--- src/main/java/de/dhbw/modellbahn/plugin/JSONConfigReader.java
+++ src/main/java/de/dhbw/modellbahn/plugin/JSONConfigReader.java
@@ -8,15 +8,12 @@ import de.dhbw.modellbahn.domain.ConfigReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class JSONConfigReader implements ConfigReader {
     private final String projectDir = System.getProperty("user.dir");
-    private final String configPath = projectDir + "/src/main/java/de/dhbw/modellbahn/config/";
+    private final String configPath = projectDir + "/src/main/resources/config/";
     private final ObjectMapper mapper = new ObjectMapper();
     private List<Integer> locIds = null;
 
@@ -24,7 +21,7 @@ public class JSONConfigReader implements ConfigReader {
     public List<Integer> getValidLocIds() {
         if (locIds == null) {
             try {
-                String file = Files.readString(Path.of(configPath + "LocIds.json"));
+                String file = Files.readString(Path.of(configPath + "locs/loc_ids.json"));
                 locIds = mapper.readValue(file, LocIdsConfig.class).getLocIds();
             } catch (IOException e) {
                 throw new RuntimeException("Could not find JSON file containing the loc ids.\n" + e);
