4ea0d05b45ad3bdb14120fd58a00ccc60d245cad
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2025-03-13 14:24:31 +0100

    Implement RoutingApplication class for locomotive routing and graph generation

---
+++ src/main/java/de/dhbw/modellbahn/RoutingApplication.java
@@ -0,0 +1,64 @@
+package de.dhbw.modellbahn;
+
+import de.dhbw.modellbahn.adapter.locomotive_reading.LocomotiveReader;
+import de.dhbw.modellbahn.adapter.moba.communication.ApiService;
+import de.dhbw.modellbahn.adapter.moba.communication.calls.LocCallsAdapter;
+import de.dhbw.modellbahn.adapter.moba.communication.calls.SystemCallsAdapter;
+import de.dhbw.modellbahn.adapter.moba.communication.calls.TrackComponentCallsAdapter;
+import de.dhbw.modellbahn.adapter.track_generation.GraphGenerator;
+import de.dhbw.modellbahn.application.RouteBuilder;
+import de.dhbw.modellbahn.application.port.moba.communication.LocCalls;
+import de.dhbw.modellbahn.application.port.moba.communication.SystemCalls;
+import de.dhbw.modellbahn.application.port.moba.communication.TrackComponentCalls;
+import de.dhbw.modellbahn.application.routing.PathNotPossibleException;
+import de.dhbw.modellbahn.application.routing.Route;
+import de.dhbw.modellbahn.domain.ConfigReader;
+import de.dhbw.modellbahn.domain.graph.Graph;
+import de.dhbw.modellbahn.domain.graph.GraphPoint;
+import de.dhbw.modellbahn.domain.locomotive.LocId;
+import de.dhbw.modellbahn.domain.locomotive.Locomotive;
+import de.dhbw.modellbahn.plugin.YAMLConfigReader;
+import de.dhbw.modellbahn.plugin.routing.jgrapht.RouteBuilderForJGraphT;
+
+import java.util.List;
+
+class RoutingApplication {
+    private final List<Locomotive> locomotiveList;
+    private final ConfigReader configReader;
+    private final Graph domainGraph;
+
+    public RoutingApplication() {
+        configReader = new YAMLConfigReader();
+
+        ApiService apiService = new ApiService(25438); // just a random number - stolen from central station
+        LocCalls locCalls = new LocCallsAdapter(apiService);
+        SystemCalls systemCalls = new SystemCallsAdapter(apiService);
+
+        TrackComponentCalls trackComponentCalls = new TrackComponentCallsAdapter(apiService);
+        domainGraph = new GraphGenerator(configReader, trackComponentCalls).generateGraph();
+        locomotiveList = new LocomotiveReader(configReader).readLocomotives();
+    }
+
+    public static void main(String[] args) {
+        RoutingApplication app = new RoutingApplication();
+        try {
+            app.driveLocomotive("16389", "Bahnhof 1");
+        } catch (PathNotPossibleException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void driveLocomotive(String locomotiveId, String destinationName) throws PathNotPossibleException {
+        int locomotiveIdInt = Integer.parseInt(locomotiveId);
+        LocId locId = new LocId(locomotiveIdInt, configReader);
+        Locomotive loc = locomotiveList.stream().filter(l -> l.getLocId().equals(locId)).findFirst().orElseThrow();
+
+        GraphPoint destination = GraphPoint.of(destinationName);
+        RouteBuilder builder = new RouteBuilderForJGraphT(domainGraph).addLocomotive(loc).considerElectrification(false).setDestinationForLoc(loc, destination);
+
+        builder.generateRoute();
+        Route route = builder.getRouteForLoc(loc);
+
+        route.driveRoute();
+    }
+}
