4320922ec875c5c4ced2457addee8a5d43071539
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2025-03-15 11:55:25 +0100

    Refactor LocId class: change to final class, update constructor, and rename methods for clarity

--- src/main/java/de/dhbw/modellbahn/domain/locomotive/LocId.java
+++ src/main/java/de/dhbw/modellbahn/domain/locomotive/LocId.java
@@ -3,29 +3,30 @@ package de.dhbw.modellbahn.domain.locomotive;
 import de.dhbw.modellbahn.domain.ConfigReader;
 
 import java.util.List;
-import java.util.Objects;
 
-public record LocId(int id, ConfigReader configReader) {
-    public LocId {
+public final class LocId {
+    private final int id;
+
+    public LocId(int id, ConfigReader configReader) {
         List<Integer> validIds = configReader.getValidLocIds();
         if (!validIds.contains(id)) {
             throw new IllegalArgumentException("Id " + id + " is not a valid loc id.");
         }
+        this.id = id;
     }
 
-    public boolean equals(int id) {
+    public boolean hasId(int id) {
         return this.id == id;
     }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        LocId locId = (LocId) o;
-        return id() == locId.id();
+    public int id() {
+        return id;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hashCode(id());
+    public String toString() {
+        return "LocId[" +
+                "id=" + id + ']';
     }
+
 }
