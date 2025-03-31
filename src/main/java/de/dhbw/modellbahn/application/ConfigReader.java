2bfdcdcced9cae12377179ce09f044d520c435b6
MichalskyL <MichalskyL@gmx.de>
2025-03-31 12:36:55 +0200

    added method updateLocomotives to ConfigReader

--- src/main/java/de/dhbw/modellbahn/application/ConfigReader.java
+++ src/main/java/de/dhbw/modellbahn/application/ConfigReader.java
@@ -3,8 +3,11 @@ package de.dhbw.modellbahn.application;
 import de.dhbw.modellbahn.adapter.locomotive.reading.ConfigLocomotive;
 import de.dhbw.modellbahn.adapter.physical.railway.communication.ApiConfig;
 import de.dhbw.modellbahn.adapter.track.generation.*;
+import de.dhbw.modellbahn.domain.locomotive.Locomotive;
+import de.dhbw.modellbahn.domain.locomotive.attributes.LocId;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ConfigReader {
 
@@ -27,4 +30,6 @@ public interface ConfigReader {
     List<ConfigBufferStop> getBufferStops();
 
     List<ConfigSignal> getSignals();
+
+    void updateLocomotives(Map<LocId, Locomotive> locomotiveMap);
 }
