d7b2ad33b8e003254ee9196657e2cf122acfa04b
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2025-04-14 12:39:27 +0200

    Removed issues with track contacts
    should have been removed in earlier commits

--- src/main/java/de/dhbw/modellbahn/parser/CommandExecutor.java
+++ src/main/java/de/dhbw/modellbahn/parser/CommandExecutor.java
@@ -4,10 +4,12 @@ import de.dhbw.modellbahn.application.repositories.LocomotiveRepository;
 import de.dhbw.modellbahn.domain.graph.Graph;
 import de.dhbw.modellbahn.domain.physical.railway.communication.SystemCalls;
 import de.dhbw.modellbahn.parser.instructions.Instruction;
+import de.dhbw.modellbahn.parser.instructions.RemoveLocomotiveFromTrackInstr;
 import de.dhbw.modellbahn.parser.lexer.CommandContext;
 
 import java.io.PrintStream;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 public class CommandExecutor {
     private final CommandContext commandContext;
@@ -17,11 +19,31 @@ public class CommandExecutor {
     }
 
     public void execute(List<Instruction> instructions, boolean trace) throws Exception {
-        for (Instruction instruction : instructions) {
-            if (trace) {
-                instruction.trace(commandContext);
+        try {
+            for (Instruction instruction : instructions) {
+                if (trace) {
+                    instruction.trace(commandContext);
+                }
+                instruction.execute(commandContext);
             }
-            instruction.execute(commandContext);
+        } catch (Exception e) {
+            commandContext.getSystemCalls().systemStop();
+            commandContext.getOutput().println("Stopped System due to error during execution of last instruction > " + e.getMessage());
+            setAllLocomotivesNotOnTrack();
+            throw e;
+        }
+
+    }
+
+    private void setAllLocomotivesNotOnTrack() throws Exception {
+        List<RemoveLocomotiveFromTrackInstr> removeInstr = StreamSupport.stream(commandContext.getLocInfos().spliterator(), false)
+                .map(RemoveLocomotiveFromTrackInstr::new).toList();
+        for (Instruction inst : removeInstr) {
+            try {
+                inst.execute(commandContext);
+            } catch (Exception e) {
+                throw new Exception("Failed to set all locomotives not on track", e);
+            }
         }
     }
 }
